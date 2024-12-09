b4cfb661f5ef5ef47eae382cafc7cb51deccd3d5
AngelicaMorale <101543485@georgebrown.ca>
2024-12-09 01:53:56 +0000

    phone testing

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-// JavaScript para el despliegue del menú desplegable
-const dropdownToggle = document.querySelector('.dropdown-toggle');
-const dropdownMenu = document.querySelector('.dropdown-menu');
 
-dropdownToggle.addEventListener('click', (e) => {
-  e.preventDefault();
-  dropdownMenu.classList.toggle('show');
-});
-
-// Cerrar el menú desplegable al hacer clic fuera de él
-document.addEventListener('click', (e) => {
-  if (!dropdownToggle.contains(e.target) && !dropdownMenu.contains(e.target)) {
-    dropdownMenu.classList.remove('show');
-  }
-  
-});
 
